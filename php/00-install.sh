4c1ba3ef37ae60cbfe73e71708744cb960480952
Nick Aguilar H <nickaguilarh@gmail.com>
2020-04-02 01:04:44 -0500

    install required extensions

--- php/00-install.sh
+++ php/00-install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-apk add bzip2 file re2c freetds freetype icu libintl libldap libjpeg libmcrypt libpng libpq libwebp libzip
+apk add automake  libtool pkgconfig bzip2 file re2c freetds freetype icu libintl libldap libjpeg libmcrypt libpng libpq libwebp libzip imagemagick
 
-TMP="autoconf bzip2-dev freetds-dev freetype-dev g++ gcc gettext-dev icu-dev jpeg-dev libmcrypt-dev libpng-dev libwebp-dev libxml2-dev libzip-dev make openldap-dev postgresql-dev"
+TMP="autoconf bzip2-dev freetds-dev freetype-dev g++ gcc gettext-dev icu-dev jpeg-dev libmcrypt-dev libpng-dev libwebp-dev libxml2-dev libzip-dev make openldap-dev postgresql-dev zlib-dev libjpeg-turbo-dev libxslt-dev gmp-dev imagemagick-dev"
 apk add $TMP
 
 # Configure extensions
@@ -19,8 +19,10 @@ docker-php-ext-install \
     ldap \
     pdo_dblib \
     pdo_mysql \
-    pdo_pgsql \
     xmlrpc \
+    soap \
+    bcmath \
+    pcntl \
     zip
 
 # Download trusted certs
@@ -31,6 +33,9 @@ rm -rf /var/cache/apk/* /tmp/*
 cd /tmp && php -r "readfile('https://getcomposer.org/installer');" | php && \
 	mv composer.phar /usr/bin/composer && \
 	chmod +x /usr/bin/composer
+
+pecl install imagick
+docker-php-ext-enable imagick
 
 apk del $TMP
 
